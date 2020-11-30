4472578fb8e3a690960a115349b374bf668c3a56
aag22 <Pololete17+>
2020-11-30 14:31:18 +0100

    python 3.7.2 no soporta 3.7

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-mkdir -p ~/.streamlit/
-
-echo "[general]\nemail = \"your-email@domain.com\"\n" > ~/.streamlit/credentials.toml
-
-echo "[server]\nheadless = true\nenableCORS=false\nport = $PORT\n" > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
